6fc80441fdb2dcbcf60dbf9de982644ee8591209
Bierwolf <l.bierwolf@web.de>
2017-11-23 11:09:50 +0100

    Lösung Aufgabe 6

--- MariaDb/src/mariadb.java
+++ MariaDb/src/mariadb.java
@@ -1,26 +1,34 @@
 import java.sql.*;
+import java.util.Scanner;
 
 public class mariadb {
 	public static void main (String args[]) 
 	{
 		try 
 		{
+			Scanner sc = new Scanner(System.in);
 			Connection conn = DriverManager.getConnection("jdbc:mariadb://127.0.0.1:3306/test", "dbi", "dbi_pass");
 			Statement stmt = conn.createStatement();
+			System.out.println("Bitte ProduktID angeben");
+			String pid = sc.nextLine();
+			sc.close();
 			ResultSet rs = stmt.executeQuery
 					(
-					"select distinct ordno, pid " +
+					"select distinct aid, sum(dollars) as sum " +
 					"from orders " +
-					"where qty >= 1000"
+					"where pid = " + "'" + pid + "'" +
+					" group by aid " +
+					"order by sum desc;"
 					);
 			while(rs.next()) 
 			{
-				System.out.println(rs.getString("ordno") + " " + rs.getString("pid") );
+				System.out.println(rs.getString("aid") + " " + rs.getString("sum") );
 			}
+			conn.close();
 		} catch (SQLException e) 
 		{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
+			}
 	}
 }
