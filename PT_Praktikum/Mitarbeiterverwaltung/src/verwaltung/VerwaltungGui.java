75fd864591f18712a05cfb89ece6612ce49a00bb
Bierwolf <l.bierwolf@web.de>
2018-01-26 12:18:57 +0100

    AddMitarbeiter
    Mitarbeiter werden nun permanent zur Verwaltung geaddet

--- PT_Praktikum/Mitarbeiterverwaltung/src/verwaltung/VerwaltungGui.java
+++ PT_Praktikum/Mitarbeiterverwaltung/src/verwaltung/VerwaltungGui.java
@@ -3,11 +3,13 @@ package verwaltung;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.time.LocalDate;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JTextPane;
 import javax.swing.table.*;
 
 import personal.*;
@@ -23,6 +25,7 @@ public class VerwaltungGui {
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
+		@SuppressWarnings("unused")
 		Personalverwaltung pw = new Personalverwaltung();
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -73,28 +76,30 @@ public class VerwaltungGui {
 		}
 		
 		JButton addButton = new JButton("Add");
-		 addButton.setBounds(0, 0, 128, 20);
-		 frame.getContentPane().add(addButton);
-		    addButton.addActionListener(new ActionListener() {
+		addButton.setBounds(0, 0, 128, 20);
+		frame.getContentPane().add(addButton);
+		addButton.addActionListener(new ActionListener() {
 
-		      public void actionPerformed(ActionEvent event) {
-		        String[] Mitarbeiter = { "", "", "" };
-		        model.addRow(Mitarbeiter);
-		        //doppelte loeschen und sortieren
-		        Personalverwaltung.removedoubles();
-		        Collections.sort(Personalverwaltung.Verwaltung);
-		      }
-		    });
+			public void actionPerformed(ActionEvent event) {
+//				String[] Mitarbeiter = { "Kevin", "200", "40" };
+//				model.addRow(Mitarbeiter);
+				Personalverwaltung.Verwaltung.add(new Manager("Bob", 30, 40));
+				//doppelte loeschen und sortieren
+				Personalverwaltung.removedoubles();
+				Collections.sort(Personalverwaltung.Verwaltung);
+				model.addRow(new Object[] {"Bob", LocalDate.now(), 30});
+		    }
+		 });
 
 		 JButton removeButton = new JButton("Remove");
 		 removeButton.setBounds(128, 0, 128, 20);
 		 frame.getContentPane().add(removeButton);
-		 	removeButton.addActionListener(new ActionListener() {
+		 removeButton.addActionListener(new ActionListener() {
 
-		      public void actionPerformed(ActionEvent event) {
-		        if(table.getSelectedRow() != -1)
-		    	  model.removeRow(table.getSelectedRow());
-		      }
-		    });
+			 public void actionPerformed(ActionEvent event) {
+				 if(table.getSelectedRow() != -1)
+					 model.removeRow(table.getSelectedRow());
+			 }
+		 });
 	}
 }
