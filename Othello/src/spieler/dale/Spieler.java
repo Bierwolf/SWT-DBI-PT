ec6c34afc4926aa155db7ae82f7c8301eaafee63
DanielG1997 <grossenbachdaniel@gmail.com>
2018-05-21 15:07:40 +0200

    Othello fix

--- Othello/src/spieler/dale/Spieler.java
+++ Othello/src/spieler/dale/Spieler.java
@@ -4,9 +4,23 @@ import spieler.*;
 
 public class Spieler implements OthelloSpieler
 {
-//	Brett brett;
+	static int anzahl = 8;
+	private Farbe [][] brett = new Farbe [anzahl][anzahl];
 	Farbe ich;
 	Farbe gegner;
+	
+	private void neuesBrett()
+	  {
+	    for (int zeile = 0; zeile < 8; zeile++)
+	    {
+	      for (int spalte = 0; spalte < 8; spalte++)
+	        brett[zeile][spalte] = Farbe.LEER;
+	    }
+	    brett[3][3] = Farbe.WEISS;
+	    brett[3][4] = Farbe.SCHWARZ;
+	    brett[4][3] = Farbe.SCHWARZ;
+	    brett[4][4] = Farbe.WEISS;
+	  }
 	
 	@Override
 	 public Zug berechneZug(Zug vorherigerZug, long zeitWeiss,
@@ -27,9 +41,7 @@ public class Spieler implements OthelloSpieler
 		 gegner = Farbe.WEISS;
 		 if (ich == Farbe.WEISS)
 			 gegner = Farbe.SCHWARZ;
-		 
-//		 brett = new Brett();
-//		 brett.initialisiereBrett();
+		 neuesBrett();
 	 }
 	 
 	 @Override
