4e17a9545d8d392d483b2eabc3efc8c97e114760
Bierwolf <l.bierwolf@web.de>
2018-06-05 09:57:19 +0200

    erste rekursion

--- Othello/src/spieler/dale/Spieler.java
+++ Othello/src/spieler/dale/Spieler.java
@@ -10,6 +10,7 @@ public class Spieler implements OthelloSpieler
 	public Spieler() {};
 	
 	static int groesse = 8;
+	int Rekursionstiefe = 4;
 	private Farbe [][] brett = new Farbe [groesse][groesse];
 	private int[][] Values = { 	{30,-4,12,10,10,12,-4,30},
 								{-4,-8,-4,2,2,-4,-8,-4},
@@ -20,6 +21,7 @@ public class Spieler implements OthelloSpieler
 								{-4,-8,-4,2,2,-4,-8,-4},
 								{30,-4,12,10,10,12,-4,30}			
 								};
+	private int PassValue = -6;
 	Farbe ich;
 	Farbe gegner;
 	
@@ -43,10 +45,10 @@ public class Spieler implements OthelloSpieler
 	 {
 		ArrayList<Zug> ZugListe = new ArrayList<Zug>();
 		if(vorherigerZug != null && vorherigerZug.getPassen() != true)
-			aktualisiereBrett(vorherigerZug.getZeile(), vorherigerZug.getSpalte(), gegner, ich);
+			brett = aktualisiereBrett(brett, vorherigerZug.getZeile(), vorherigerZug.getSpalte(), gegner, ich);
 		for(int i = 0; i <= 7; i++) {
 			for(int j = 0; j <= 7; j++) {
-				if(legalerZug(i,j)) {
+				if(legalerZug(brett, i, j, ich, gegner)) {
 					ZugListe.add(new Zug(i,j));					
 					
 				}
@@ -64,16 +66,84 @@ public class Spieler implements OthelloSpieler
 			if(Zug.getZeile() == -1)
 			{
 				Zug = z;
-			}else if(
-				Values[z.getZeile()][z.getSpalte()] > Values[Zug.getZeile()][Zug.getSpalte()]) {
+			}else if(getValue(z) > getValue(Zug)) {
 				Zug = z;
 			}
 			
 		}
-		aktualisiereBrett(Zug.getZeile(),Zug.getSpalte(), ich, gegner);
+
+		//ArrayList<Zug> besterPfad= berechneNächsterZug(brett, ich, gegner, 0, new ArrayList<Zug>());
+		brett = aktualisiereBrett(brett, Zug.getZeile(),Zug.getSpalte(), ich, gegner);
+		//return besterPfad.get(besterPfad.size()-1);
 		return Zug;
 		
 	 }
+	
+	public ArrayList<Zug> berechneNächsterZug(Farbe[][] brett, Farbe ich, Farbe gegner, int Tiefe, ArrayList<Zug> BesterPfad) {
+		ArrayList<Zug> ZugListe = new ArrayList<Zug>();
+		for(int i = 0; i <= 7; i++) {
+			for(int j = 0; j <= 7; j++) {
+				if(legalerZug(brett, i, j, ich, gegner)) {
+					ZugListe.add(new Zug(i,j));					
+					
+				}
+			}
+			
+		}
+		
+		if(ZugListe.isEmpty()) {
+			Zug Pass = new Zug(-1,-1);
+			Pass.setPassen();
+			BesterPfad.add(Pass);
+			return BesterPfad;
+		}
+				
+		if(Tiefe == Rekursionstiefe) {
+			// TODO Passen benötigt noch dringende einbindung in die vorrausrechnung
+			
+			Zug Zug = new Zug(-5,-5);
+			for(Zug z : ZugListe) {
+				if(Zug.getZeile() == -5)
+				{
+					Zug = z;
+				}else if(getValue(z) > getValue(Zug)) {
+					Zug = z;
+				}
+			}
+			BesterPfad.add(Zug);
+			return BesterPfad;
+		}else {
+			Zug besterZug = new Zug(-1,-1);
+			int Value = -1000;
+			for(Zug y : ZugListe) {
+				int tempValue=0;
+				int flipper = 1;
+				if((Tiefe&1) == 1) {
+					flipper = -1;
+				}
+				for(Zug z : berechneNächsterZug(aktualisiereBrett(brett, y.getZeile(), y.getSpalte(), ich, gegner), gegner, ich, Tiefe++, BesterPfad)) {
+					tempValue += getValue(z)*flipper;
+					flipper *= -1;
+				}
+				if(tempValue > Value) {
+					besterZug = y;
+					Value = tempValue;
+				}
+			}
+			BesterPfad.add(besterZug);
+			return BesterPfad;
+		}
+		
+	}
+	
+	public int getValue(Zug z) {
+		if(z.getPassen() == true) {
+			return PassValue;
+		}else {
+			return Values[z.getZeile()][z.getSpalte()];
+		}
+	}
+
 	
 	 @Override
 	 public void neuesSpiel(Farbe meineFarbe,
@@ -86,7 +156,7 @@ public class Spieler implements OthelloSpieler
 		 neuesBrett();
 	 }
 
-	 public void aktualisiereBrett(int z, int s, Farbe ich, Farbe gegner) {
+	 public Farbe[][] aktualisiereBrett(Farbe[][] brett, int z, int s, Farbe ich, Farbe gegner) {
 		 if(brett[z][s] == Farbe.LEER) {
 			 //rechts
 			 for(int i = 1; s+i <= 7;) {
@@ -217,9 +287,10 @@ public class Spieler implements OthelloSpieler
 			 }
 			 
 		 }
+		 return brett;
 	 }
 	 
-	 public boolean legalerZug(int z, int s) {
+	 public boolean legalerZug(Farbe[][] brett, int z, int s, Farbe ich, Farbe gegner) {
 		 if(brett[z][s] == Farbe.LEER) {
 			 //rechts
 			 for(int i = 1; s+i <= 7;) {
